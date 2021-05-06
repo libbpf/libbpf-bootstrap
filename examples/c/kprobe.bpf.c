b50ea2a04531f23fe3230440330150195f356a2c
Hengqi Chen <chenhengqi@outlook.com>
2021-05-06 08:43:37 +0800

    fentry, kprobe: use process ID instead of thread ID
    Signed-off-by: Hengqi Chen <chenhengqi@outlook.com>

--- examples/c/kprobe.bpf.c
+++ examples/c/kprobe.bpf.c
@@ -10,18 +10,21 @@ char LICENSE[] SEC("license") = "Dual BSD/GPL";
 SEC("kprobe/do_unlinkat")
 int BPF_KPROBE(do_unlinkat, int dfd, struct filename *name)
 {
-	const char *filename = BPF_CORE_READ(name, name);
 	pid_t pid;
+	const char *filename;
 
-	pid = bpf_get_current_pid_tgid();
-
+	pid = bpf_get_current_pid_tgid() >> 32;
+	filename = BPF_CORE_READ(name, name);
 	bpf_printk("KPROBE ENTRY pid = %d, filename = %s\n", pid, filename);
 	return 0;
 }
 
 SEC("kretprobe/do_unlinkat")
-int BPF_KRETPROBE(do_open_execat_exit, long ret)
+int BPF_KRETPROBE(do_unlinkat_exit, long ret)
 {
-	bpf_printk("KPROBE EXIT: ret = %ld\n", ret);
+	pid_t pid;
+
+	pid = bpf_get_current_pid_tgid() >> 32;
+	bpf_printk("KPROBE EXIT: pid = %d, ret = %ld\n", pid, ret);
 	return 0;
 }
