629f20a76b094731bbaf5e8680e32946a05686b1
ruki <waruqi@gmail.com>
2021-05-18 22:37:15 +0800

    improve xmake.lua and add two options

--- examples/c/xmake.lua
+++ examples/c/xmake.lua
@@ -2,8 +2,13 @@ add_rules("mode.release", "mode.debug")
 add_rules("platform.linux.bpf")
 set_license("GPL-2.0")
 
+option("system-libbpf",      {showmenu = true, default = false, description = "Use system-installed libbpf"})
+option("require-bpftool",    {showmenu = true, default = false, description = "Require bpftool package"})
+
 add_requires("libelf", "zlib")
-add_requires("linux-tools", {configs = {bpftool = true}})
+if has_config("require-bpftool") then
+    add_requires("linux-tools", {configs = {bpftool = true}})
+end
 if is_plat("android") then
     add_requires("ndk >=22.x", "argp-standalone")
     set_toolchains("@ndk", {sdkver = "23"})
@@ -15,45 +20,69 @@ end
 
 add_includedirs("../../vmlinux")
 
-target("libbpf")
-    set_kind("static")
-    set_basename("bpf")
-    add_files("../../libbpf/src/*.c")
-    add_includedirs("../../libbpf/include")
-    add_includedirs("../../libbpf/include/uapi", {public = true})
-    add_includedirs("$(buildir)", {interface = true})
-    add_configfiles("../../libbpf/src/(*.h)", {prefixdir = "bpf"})
-    add_packages("libelf", "zlib")
-    if is_plat("android") then
-        add_defines("__user=", "__force=", "__poll_t=uint32_t")
-    end
+-- we can run `xmake f --require-bpftool=y` to pull bpftool from xmake-repo repository
+if has_config("require-bpftool") then
+    add_packages("linux-tools")
+else
+    before_build(function (target)
+        os.addenv("PATH", path.join(os.scriptdir(), "..", "..", "tools"))
+    end)
+end
+
+-- we use the vendored libbpf sources for libbpf-bootstrap.
+-- for some projects you may want to use the system-installed libbpf, so you can run `xmake f --system-libbpf=y`
+if has_config("system-libbpf") then
+    add_requires("libbpf", {system = true})
+else
+    target("libbpf")
+        set_kind("static")
+        set_basename("bpf")
+        add_files("../../libbpf/src/*.c")
+        add_includedirs("../../libbpf/include")
+        add_includedirs("../../libbpf/include/uapi", {public = true})
+        add_includedirs("$(buildir)", {interface = true})
+        add_configfiles("../../libbpf/src/(*.h)", {prefixdir = "bpf"})
+        add_packages("libelf", "zlib")
+        if is_plat("android") then
+            add_defines("__user=", "__force=", "__poll_t=uint32_t")
+        end
+end
 
 target("minimal")
     set_kind("binary")
-    add_deps("libbpf")
     add_files("minimal*.c")
-    add_packages("linux-tools", "linux-headers")
+    add_packages("linux-headers")
+    if not has_config("system-libbpf") then
+        add_deps("libbpf")
+    end
 
 target("bootstrap")
     set_kind("binary")
-    add_deps("libbpf")
     add_files("bootstrap*.c")
-    add_packages("linux-tools", "linux-headers")
+    add_packages("linux-headers")
+    if not has_config("system-libbpf") then
+        add_deps("libbpf")
+    end
     if is_plat("android") then
         add_packages("argp-standalone")
     end
 
 target("fentry")
     set_kind("binary")
-    add_deps("libbpf")
     add_files("fentry*.c")
-    add_packages("linux-tools", "linux-headers")
+    add_packages("linux-headers")
+    if not has_config("system-libbpf") then
+        add_deps("libbpf")
+    end
+
 
 target("kprobe")
     set_kind("binary")
-    add_deps("libbpf")
     add_files("kprobe*.c")
-    add_packages("linux-tools", "linux-headers")
+    add_packages("linux-headers")
+    if not has_config("system-libbpf") then
+        add_deps("libbpf")
+    end
     if is_plat("android") then
         -- TODO we need fix vmlinux.h tu support android
         set_default(false)
