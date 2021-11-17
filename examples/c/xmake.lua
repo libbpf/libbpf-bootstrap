1b9d2a53272740ae0487fb6c7001cb2cc08a9ea3
ruki <waruqi@gmail.com>
2021-11-17 10:49:32 +0800

    Update xmake.lua to error out on buggy xmake versions
    Add an error for versions of xmake that can't correctly compile *.bpf.c
    files.

--- examples/c/xmake.lua
+++ examples/c/xmake.lua
@@ -2,6 +2,12 @@ add_rules("mode.release", "mode.debug")
 add_rules("platform.linux.bpf")
 set_license("GPL-2.0")
 
+if xmake.version():satisfies(">=2.5.7 <=2.5.9") then
+    on_load(function (target)
+        raise("xmake(%s) has a bug preventing BPF source code compilation. Please run `xmake update -f 2.5.6` to revert to v2.5.6 version or upgrade to xmake v2.6.1 that fixed the issue.", xmake.version())
+    end)
+end
+
 option("system-libbpf",      {showmenu = true, default = false, description = "Use system-installed libbpf"})
 option("require-bpftool",    {showmenu = true, default = false, description = "Require bpftool package"})
 
