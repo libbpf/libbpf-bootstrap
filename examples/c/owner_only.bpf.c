827042bec38930feefc8b31ce133231fb97b98e0
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2021-12-09 09:39:44 +0530

    add owner_only POC
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>

--- examples/c/owner_only.bpf.c
+++ examples/c/owner_only.bpf.c
@@ -3,6 +3,12 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+#define	EPERM		 1
+
+char value[32];
+unsigned int pid_namespace;
+
 static bool isequal(const char *a, const char *b) {
 #pragma unroll
   for (int i = 0; i < 32; i++) {
@@ -24,11 +30,13 @@ static bool in_pid_ns(unsigned int pid_namespace, struct task_struct *t) {
   return true;
 }
 
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
-#define	EPERM		 1
-
-char value[32];
-unsigned int pid_namespace;
+static bool is_owner(struct file* file_p){
+    kuid_t owner = BPF_CORE_READ(file_p, f_inode, i_uid);
+  unsigned int z = bpf_get_current_uid_gid();
+  if (owner.val != z)
+    return false;
+  return true;
+}
 
 SEC("lsm/bprm_check_security")
 int BPF_PROG(executable_block, struct linux_binprm *bprm, int ret) {
@@ -36,14 +44,13 @@ int BPF_PROG(executable_block, struct linux_binprm *bprm, int ret) {
   if (!in_pid_ns(pid_namespace, t))
     return ret;
 
+
   if (ret != 0)
     return ret;
   char filename[32];
 
   bpf_probe_read_str(&filename, 32, bprm->filename);
-  bpf_printk("%s", filename);
-  bool comp = isequal(filename, value);
-  if (comp)
+  if (isequal(filename, value) && !is_owner(bprm->file))
     return -EPERM;
 
   return ret;
---
+++ examples/c/owner_only.c
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (c) 2020 Facebook */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <bpf/libbpf.h>
+#include <stdlib.h>
+#include "owner_only.skel.h"
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	return vfprintf(stderr, format, args);
+}
+
+static void bump_memlock_rlimit(void)
+{
+	struct rlimit rlim_new = {
+		.rlim_cur	= RLIM_INFINITY,
+		.rlim_max	= RLIM_INFINITY,
+	};
+
+	if (setrlimit(RLIMIT_MEMLOCK, &rlim_new)) {
+		fprintf(stderr, "Failed to increase RLIMIT_MEMLOCK limit!\n");
+		exit(1);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	struct owner_only_bpf *skel;
+	int err;
+
+	/* Set up libbpf errors and debug info callback */
+	libbpf_set_print(libbpf_print_fn);
+
+	/* Bump RLIMIT_MEMLOCK to allow BPF sub-system to do anything */
+	bump_memlock_rlimit();
+
+	/* Open BPF application */
+	skel = owner_only_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open BPF skeleton\n");
+		return 1;
+	}
+
+	if (argc > 2){
+		strcpy(skel->bss->value, argv[1]);
+		skel->bss->pid_namespace = (unsigned int)atoi(argv[2]);
+	}
+
+
+	/* Load & verify BPF programs */
+	err = owner_only_bpf__load(skel);
+	if (err) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+
+	/* Attach tracepoint handler */
+	err = owner_only_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` ");
+
+	for (;;) {
+		/* trigger our BPF program */
+		fprintf(stderr, ".");
+		sleep(1);
+	}
+
+cleanup:
+	owner_only_bpf__destroy(skel);
+	return -err;
+}
